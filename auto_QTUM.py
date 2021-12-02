fcf8472aebb66b6a33173208d9ae876928f84bfa
JngMkk <87686562+JngMkk@users.noreply.github.com>
2021-12-03 05:39:51 +0900

    Add files via upload

---
+++ auto_QTUM.py
@@ -0,0 +1,127 @@
+import time
+import pyupbit
+import datetime
+import requests
+from fbprophet import Prophet
+import schedule
+import numpy as np
+import pandas as pd
+
+access = ""
+secret = ""
+myToken = 'xoxb-2788648796128-2777574821169-o8KzIDWv7KhfCBBguPDEmJWV'
+
+def get_ror(ticker, k):
+    df = pyupbit.get_ohlcv(ticker, count=7)
+    df['range'] = (df['high'] - df['low']) * k
+    df['target'] = df['open'] + df['range'].shift(1)
+
+    df['ror'] = np.where(df['high'] > df['target'],
+                         df['close'] / df['target'],
+                         1)
+
+    ror = df['ror'].cumprod()[-1]
+    return ror
+
+def bestk(ticker):
+    r = []
+    hpr = []
+    for k in np.arange(0.1, 1.0, 0.1):
+        hpr.append(get_ror(ticker, k))
+        r.append(k)
+
+    goodk = r[hpr.index(max(hpr))]
+    return goodk
+
+
+def post_message(token, channel, text):
+    """슬랙 메시지 전송"""
+    response = requests.post("https://slack.com/api/chat.postMessage",
+        headers={"Authorization": "Bearer "+token},
+        data={"channel": channel,"text": text})
+
+def get_target_price(ticker):
+    """변동성 돌파 전략으로 매수 목표가 조회"""
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
+    target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * bestk(ticker)
+    print('target_price : ' , target_price)
+    return target_price
+
+def get_start_time(ticker):
+    """시작 시간 조회"""
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
+    start_time = df.index[0]
+    return start_time
+
+def get_ma15(ticker):
+    """15일 이동 평균선 조회"""
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=15)
+    ma15 = df['close'].rolling(15).mean().iloc[-1]
+    return ma15
+
+def get_balance(ticker):
+    """잔고 조회"""
+    balances = upbit.get_balances()
+    for b in balances:
+        if b['currency'] == ticker:
+            if b['balance'] is not None:
+                return float(b['balance'])
+            else:
+                return 0
+    return 0
+
+def get_current_price(ticker):
+    """현재가 조회"""
+    return pyupbit.get_orderbook(ticker=ticker)["orderbook_units"][0]["ask_price"]
+
+predicted_close_price = 0
+def predict_price(ticker):
+    """Prophet으로 당일 종가 가격 예측"""
+    global predicted_close_price
+    df = pyupbit.get_ohlcv(ticker, interval="minute60")
+    df = df.reset_index()
+    df['ds'] = df['index']
+    df['y'] = df['close']
+    data = df[['ds','y']]
+    model = Prophet()
+    model.fit(data)
+    future = model.make_future_dataframe(periods=24, freq='H')
+    forecast = model.predict(future)
+    closeDf = forecast[forecast['ds'] == forecast.iloc[-1]['ds'].replace(hour=9)]
+    if len(closeDf) == 0:
+        closeDf = forecast[forecast['ds'] == data.iloc[-1]['ds'].replace(hour=9)]
+    closeValue = closeDf['yhat'].values[0]
+    predicted_close_price = closeValue
+    print('predicted_close price :', predicted_close_price)
+predict_price("KRW-QTUM")
+schedule.every().hour.do(lambda: predict_price("KRW-QTUM"))
+
+# 로그인
+upbit = pyupbit.Upbit(access, secret)
+print("autotrade start")
+# 시작 메세지 슬랙 전송
+post_message(myToken,"#coinauto", "autotrade start")
+
+# 자동매매 시작
+while True:
+    try:
+        now = datetime.datetime.now()
+        start_time = get_start_time("KRW-QTUM")
+        end_time = start_time + datetime.timedelta(days=1)
+        schedule.run_pending()
+
+        if start_time < now < end_time - datetime.timedelta(seconds=10):
+            target_price = get_target_price("KRW-QTUM")
+            current_price = get_current_price("KRW-QTUM")
+            if target_price < current_price and current_price < predicted_close_price:
+                krw = get_balance("KRW")
+                if krw > 5000:
+                    upbit.buy_market_order("KRW-QTUM", krw*0.9995)
+        else:
+            qtum = get_balance("QTUM")
+            if qtum > 0.00008:
+                upbit.sell_market_order("KRW-QTUM", qtum*0.9995)
+        time.sleep(1)
+    except Exception as e:
+        print(e)
+        time.sleep(1)
